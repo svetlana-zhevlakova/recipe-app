305e474e8429a395f15e76e36dd1efb9ec0dcc9e
Svetlana Zhevlakova <svettkins@gmail.com>
2018-09-28 08:08:17 +0300

    added form for new recipe, updated recipe controller

--- src/main/java/guru/springframework/recipeapp/controller/RecipeController.java
+++ src/main/java/guru/springframework/recipeapp/controller/RecipeController.java
@@ -1,9 +1,12 @@
 package guru.springframework.recipeapp.controller;
 
+import guru.springframework.recipeapp.command.RecipeCommand;
 import guru.springframework.recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,8 +19,21 @@ public class RecipeController {
     }
 
     @RequestMapping("/recipe/show/{id}")
-    public String showById(@PathVariable String id, Model model){
+    public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
     }
 }
